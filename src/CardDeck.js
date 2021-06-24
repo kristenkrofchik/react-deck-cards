380ae38d4b9e23feeb98d97d2a76e3cbe0f8562e
Kristen Krofchik <k.krofchik@gmail.com>
2021-06-24 16:52:53 -0500

    working on carddeck

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./Card";
 
 const CardDeck = () => {
     const [deck, setDeck] = useState(null);
+    //use state to keep track of the cards that have been drawn
+    const [drawn, setDrawn] = useState([]); 
     
     //fetch deck data from API and load into state
     useEffect(() => {
@@ -13,8 +15,52 @@ const CardDeck = () => {
         }
         loadDeck();
       }, [setDeck])
+    
+    //pick a card from the deck (using the deck_id)
+    useEffect(() => {
+        async function drawCard() {
+            let { deck_id } = deck;
 
+            try {
+                let drawRes = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
 
+                //throw an error if there are no cards left in the deck
+                if(drawRes.data.remaining === 0) {
+                    throw new Error('You have reached the end of the deck!');
+                }
+                
+                //add card and its data to the drawn array
+                const card = drawRes.data.cards[0];
+
+                setDrawn(d => [
+                    ...d,
+                    {
+                    id: card.code,
+                    name: card.suit + " " + card.value,
+                    image: card.image
+                    }
+                ]);
+            } catch (e) {
+                alert(e)
+            };
+        };
+        drawCard();
+    }, [deck]);
+
+    const cards = drawn.map(c => (
+        <Card key={c.id} name={c.name} image={c.image} />
+      ));
+
+    return (
+        <div className="CardDeck">
+            {deck ? (
+                <button className="DeckDraw" onClick={toggleAutoDraw}>
+                {autoDraw ? "STOP" : "KEEP"} DRAWING FOR ME!
+                </button>
+                ) : null}
+            <div className="CardDeckArea">{cards}</div>
+        </div>
+    )
 }
 
 export default CardDeck;
