f14c7a5522ac60787b6892a8f22473e84a10929b
Kristen Krofchik <k.krofchik@gmail.com>
2021-06-24 18:09:37 -0500

    changes to make autodraw work

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Card from "./Card";
 
@@ -6,17 +6,20 @@ const CardDeck = () => {
     const [deck, setDeck] = useState(null);
     //use state to keep track of the cards that have been drawn
     const [drawn, setDrawn] = useState([]); 
+    const [autoDraw, setAutoDraw] = useState(false);
+    const timerRef = useRef(null);
     
     //fetch deck data from API and load into state
     useEffect(() => {
         async function loadDeck() {
-          const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+          let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
           setDeck(res.data);
         }
         loadDeck();
       }, [setDeck])
     
     //pick a card from the deck (using the deck_id)
+    //draw a card every second if auto-draw functionality is toggled on
     useEffect(() => {
         async function drawCard() {
             let { deck_id } = deck;
@@ -26,6 +29,7 @@ const CardDeck = () => {
 
                 //throw an error if there are no cards left in the deck
                 if(drawRes.data.remaining === 0) {
+                    setAutoDraw(false);
                     throw new Error('Error: no cards remaining!');
                 }
                 
@@ -44,22 +48,32 @@ const CardDeck = () => {
                 alert(e)
             };
         };
-        drawCard();
-    }, [deck]);
+
+        if (autoDraw && !timerRef.current) {
+            timerRef.current = setInterval(async () => {
+              await drawCard();
+            }, 1000);
+          }
+    }, [autoDraw, setAutoDraw, deck]);
+
+    const toggleAutoDraw = () => {
+        setAutoDraw(auto => !auto);
+      };
 
     const cards = drawn.map(c => (
         <Card key={c.id} name={c.name} image={c.image} />
       ));
 
     return (
-        <div className="CardDeck">
-            {deck ? (
-                <button className="DeckDraw" onClick={toggleAutoDraw}> DRAWING FOR ME!
-                </button>
-                ) : null}
-            <div className="CardDeckArea">{cards}</div>
+        <div className="Deck">
+          {deck ? (
+            <button className="Deck-gimme" onClick={toggleAutoDraw}>
+              {autoDraw ? "STOP" : "KEEP"} DRAWING FOR ME!
+            </button>
+          ) : null}
+          <div className="Deck-cardarea">{cards}</div>
         </div>
-    )
+      );
 }
 
 export default CardDeck;
